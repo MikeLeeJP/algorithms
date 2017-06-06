3ef927061fc9b6876dc9f35ec67b57b82e5f00c9
Mike Lee <mayjinwoo@naver.com>
2017-06-05 19:14:41 -0700

    solution59

--- yourSolution.js
+++ yourSolution.js
@@ -1,9 +1,21 @@
 // Name
 // Problem Description
 
-const solution = () => {
+const solution  = (n) => {
+  if(n<=2){
+    return false;
+  }
+  else{
+    for(let i =2;i<n;i++){
+      if(n%i==0){
+	return false;
+      }
+    }
+    return true;	
+  }
 };
 
 module.exports = {
   solution
 };
+
